1548c144c173586a0c3761bd4778f063fe0d03a3
Igor Tovstopyat <itovstopyat@carelogistics.com>
2017-11-09 13:52:27 -0500

    Added Functor fproduct.

--- src/main/scala/functor/FunctorOps.scala
+++ src/main/scala/functor/FunctorOps.scala
@@ -4,14 +4,29 @@ object FunctorOps {
 
   import cats.Functor
 
-  /************************
-   * See tests for examples
-   ************************/
+  /** ************************************************************
+    * The implicit instances of Functor[Option] and Functor[List]
+    * are defined in the package object.
+    *
+    * See the tests for examples.
+    * ************************************************************/
+
 
   /*
    * lift *** Lift a function to operate on functors.
    */
   val lenOption: Option[String] => Option[Int] = Functor[Option].lift(_.length)
 
+  /*
+   * void *** Remove the values but preserve the structure
+   */
+  val voidList: List[Int] => List[Unit] = Functor[List].void(_)
+
+
+  /*
+   * fproduct *** Tuple the values with the results of applying a function
+   */
+  val tupleStrLenList: List[String] => List[(String,Int)] =
+    Functor[List].fproduct(_)((s: String) => s.length)
 }
 
--- src/test/scala/functor/FunctorSpec.scala
+++ src/test/scala/functor/FunctorSpec.scala
@@ -1,19 +1,25 @@
 package functor
 
-import org.scalatest.FlatSpec
 import functor.FunctorOps._
+import org.scalatest.FlatSpec
 
 class FunctorSpec extends FlatSpec {
 
   "lenOption(Some(\"abc\")" should "be equal to Some(3)" in {
-    assert(
+    assert {
       lenOption(Some("abc")).contains(3)
-    )
+    }
   }
 
   "lenOption(None)" should "be equal to None" in {
-    assert(
+    assert {
       lenOption(None).isEmpty
-    )
+    }
+  }
+
+  "tupleStrLenList [\"abc\", \"abcd\"]" should "be equal to [(\"abc\",3), (\"abcd\",4)]" in {
+    assert {
+      tupleStrLenList(List("John", "Lennon")) == List(("John",4), ("Lennon",6))
+    }
   }
 }
