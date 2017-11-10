82766d56124b29db10014c4cca2a2393e91543a3
Igor Tovstopyat <itovstopyat@carelogistics.com>
2017-11-10 11:24:57 -0500

    KVStore: lift DSL into monad.

--- src/main/scala/free/kvstore/KVStoreA.scala
+++ src/main/scala/free/kvstore/KVStoreA.scala
@@ -4,3 +4,23 @@ sealed trait KVStoreA[A]
 case class Put[T](key: String, value: T) extends KVStoreA[Unit]
 case class Get[T](key: String) extends KVStoreA[Option[T]]
 case class Delete(key: String) extends KVStoreA[Unit]
+
+object Ops {
+  import cats.free.Free.liftF
+
+  def put[T](key: String, value: T): KVStore[Unit] =
+    liftF[KVStoreA, Unit](Put[T](key, value))
+
+  def get[T](key: String): KVStore[Option[T]] =
+    liftF[KVStoreA, Option[T]](Get[T](key))
+
+  def delete(key: String): KVStore[Unit] =
+    liftF[KVStoreA, Unit](Delete(key))
+
+  // update composes get and set
+  def update[T](key: String, f: T => T): KVStore[Unit] =
+    for {
+      vMaybe <- get[T](key)
+      _ <- vMaybe.map(v => put[T](key, f(v)))
+    } yield ()
+}
